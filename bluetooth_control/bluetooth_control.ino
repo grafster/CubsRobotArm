b8b89266ef75ab8f9a432b783a905e0d238efbc7
graf <grafhama at yahoo co uk>
2016-11-23 23:53:11 +0000

    added support for gradually moving the arm

--- bluetooth_control/bluetooth_control.ino
+++ bluetooth_control/bluetooth_control.ino
@@ -6,6 +6,11 @@ Servo myservo2;  // create servo object to control a servo
 Servo myservo3;  // create servo object to control a servo
 Servo myservo4;  // create servo object to control a servo
 String inString = "";    // string to hold input
+int servo1Angle = 90;
+int servo2Angle = 90;
+int servo3Angle = 90; 
+int servo4Angle = 90;
+int delayTime = 10;
 
 void setup() {
   // Servos attached on pins 8 - 11
@@ -14,7 +19,7 @@ void setup() {
   myservo3.attach(10);
   myservo4.attach(11);
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -23,9 +28,51 @@ void setup() {
   myservo2.write(90);
   myservo3.write(90);
   myservo4.write(90);
-    
+  delay(500);  
+  myservo1.write(120);
+  myservo2.write(120);
+  myservo3.write(120);
+  myservo4.write(120);
+  delay(500);  
+  myservo1.write(90);
+  myservo2.write(90);
+  myservo3.write(90);
+  myservo4.write(90);
+  delay(500);  
 }
 
+void moveServoToAngle(Servo& servo, int& currentAngle, const int targetAngle)
+{
+  Serial.print("Moving to angle ");
+  Serial.print (targetAngle);
+  Serial.print (" current angle = ");
+  Serial.print(currentAngle);
+  Serial.print (" target angle = ");
+  Serial.println(targetAngle);
+  if (currentAngle < targetAngle)
+  {
+    while (currentAngle < targetAngle)
+    {
+      currentAngle++;
+    
+      servo.write(currentAngle);
+      delay(delayTime);
+    }
+  }
+  else 
+  {
+    while (currentAngle > targetAngle)
+    {
+      currentAngle--;
+    
+      servo.write(currentAngle);
+      delay(delayTime);
+    }
+  
+  }
+  Serial.println("Move Complete");
+  
+}
 void loop() {
   // Read serial input:
 
@@ -50,17 +97,29 @@ void loop() {
         }
 
         if (servoNumber == 1)
-          myservo1.write(angleString.toInt());
+        {
+          moveServoToAngle(myservo1, servo1Angle, angleString.toInt());         
+        }
         else if (servoNumber == 2)
-          myservo2.write(angleString.toInt());
+        {
+          moveServoToAngle(myservo2, servo2Angle, angleString.toInt());         
+        }
         else if (servoNumber == 3)
-          myservo3.write(angleString.toInt());
+        {
+          moveServoToAngle(myservo3, servo3Angle, angleString.toInt());         
+        }
         else if (servoNumber == 4)
-          myservo4.write(angleString.toInt());
+        {
+          moveServoToAngle(myservo4, servo4Angle, angleString.toInt());         
+        }
       }
       else if (inString[0] == 'p'){
         delay(1000);
       }
+      else if (inString[0] == 'd'){
+        delayTime = inString.substring(1).toInt();
+      }
+      
 
       inString = "";
     }
